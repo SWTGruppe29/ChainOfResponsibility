ac7c49f7fa78ef420de6712d26ab67d42c9772af
Oskarthansen <oskarthansen@outlook.dk>
2019-04-11 14:01:14 +0200

    Added interface

--- ATMEnhanced/ATMEnhanced/Classes/Decoder.cs
+++ ATMEnhanced/ATMEnhanced/Classes/Decoder.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATMEnhanced.Interfaces;
 
 namespace ATMEnhanced.Classes
 {
-    class Decoder
+    public class Decoder : Handler, IDecoder
     {
+
     }
 }
