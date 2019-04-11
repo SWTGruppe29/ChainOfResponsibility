e0ad35e3fbb3d462d31c1abade3eda0432e54aff
Mathias <math084m@hotmail.com>
2019-04-11 15:38:44 +0200

    ´RegionFilter og Trackupdater

--- ATMEnhanced/ATMEnhanced/Classes/RegionFilter.cs
+++ ATMEnhanced/ATMEnhanced/Classes/RegionFilter.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Classes;
+using ATM.Interfaces;
+using ATMEnhanced.Interfaces;
 
 namespace ATMEnhanced.Classes
 {
-    class RegionFilter : Handler
+    class RegionFilter : Handler, IRegionFilter
     {
-        protected override void Handle(TrackData data)
+        private IAirSpace _airSpace;
+        List<Track> _trackData = new List<Track>();
+
+        protected override void Handle(object data)
         {
-            
+            TrackData trackData = (TrackData)data;
+
+            _trackData = trackData.Tracks;
+
+            foreach (var track in _trackData)
+            {
+                if(!_airSpace.IsInAirSpace(track.XCoordinate,track.YCoordinate,track.Altitude))
+                {
+                    int index = CheckIfTrackIsInList(track.Tag);
+                    _trackData.RemoveAt(index);
+                    
+                }
+            }
+
+            base.Handle(_trackData);
+        }
+
+        public int CheckIfTrackIsInList(string tag)
+        {
+            for (int i = 0; i < _trackData.Count; i++)
+            {
+                if (_trackData[i].Tag == tag)
+                    return i;
+            }
+
+            return -1;
         }
     }
 }
