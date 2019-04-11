c95e4061eb408eb0ea2bbc090a54ce5c3c68b650
Mathias <math084m@hotmail.com>
2019-04-11 14:00:18 +0200

    væk med atm så prøver vi igen

---
+++ ATMEnhanced/ATMEnhanced/Interfaces/IATMFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Classes;
+using TransponderReceiver;
+
+namespace ATM.Interfaces
+{
+    public interface IATMFactory
+    {
+        IAirSpace CreateAirSpace();
+        ICondition CreateCondition();
+        IConsolePrinter CreateConsolePrinter();
+        ILogger CreateLogger();
+        ISeparationChecker CreateSeparationChecker();
+    }
+}
