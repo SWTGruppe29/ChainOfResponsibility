e4248a550f62823b3951236ba3afa1289d3ebb3d
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-04-11 13:41:02 +0200

    asfa

---
+++ CodeExample/ATMsystem/ATMsystem/Classes/Handler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMsystem.Classes
+{
+    abstract class Handler
+    {
+        protected Handler _sucessor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            _sucessor = successor;
+        }
+
+        protected void HandleRequest()
+        {
+            _sucessor?.HandleRequest();
+        }
+    }
+}
