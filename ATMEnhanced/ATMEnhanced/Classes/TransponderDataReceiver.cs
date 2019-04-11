c7653282b4aa06f648c6a9dbe88f8ecf9ea1e5eb
Oskarthansen <oskarthansen@outlook.dk>
2019-04-11 14:48:46 +0200

    Changes

---
+++ ATMEnhanced/ATMEnhanced/Classes/TransponderDataReceiver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMEnhanced.Interfaces;
+using TransponderReceiver;
+
+namespace ATMEnhanced.Classes
+{
+    public class TransponderDataReceiver : Handler, ITransponderDataReceiver
+    {
+        private ITransponderReceiver _transponderReceiver;
+        private Decoder _decoder;
+
+        public TransponderDataReceiver(ITransponderReceiver transponderReceiver)
+        {
+            _decoder = new Decoder();
+            _transponderReceiver = transponderReceiver;
+            // Subscribing to event
+            _transponderReceiver.TransponderDataReady += ReceiverTransponderReady;
+        }
+
+
+        public void ReceiverTransponderReady(object sender, RawTransponderDataEventArgs e)
+        {
+            if (e.TransponderData.Count > 0)
+            {
+                base.Handle();
+            }
+        }
+
+    }
+}
